822237b3db02f76b1e3ea2e57990d102b038b5f7
Observatoire du Salafisme <observatoire.salafisme@gmail.com>
2015-11-17 16:07:03 +0100

    arte réactivée

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <body>
 	<?php include("header.php"); ?>
 	<section class="nobottommargin">
-		<!--<iframe src="https://www.google.com/maps/d/embed?mid=zf_Jz1KgRwjo.ky1pHnqkQGuA" />
-	--></section>
+		<iframe src="https://www.google.com/maps/d/embed?mid=zf_Jz1KgRwjo.ky1pHnqkQGuA" />
+	</section>
 </body>
 </html>
